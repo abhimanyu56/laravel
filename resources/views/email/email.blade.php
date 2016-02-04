7c027360db1fe0ed8f7686a3a6dedc41b7bd9718
Abhimanyu Singh <abhimanyu56@gmail.com>
2016-02-04 19:36:54 +0530

    Final Commit

---
+++ resources/views/email/email.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang=&quot;en-US&quot;>
+<head>
+<meta charset=&quot;utf-8&quot;>
+</head>
+<body>
+<h2>Enquiry</h2>
+<div>
+    Someone just looked at <b>{{$academy_name}}</b></div>
+</body>
+</html>
